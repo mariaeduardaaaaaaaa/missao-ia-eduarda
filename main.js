d3ab1fe4f99333a3c094a77179399e8c6413b517
mariaeduardaaaaaaaa <146455966+mariaeduardaaaaaaaa@users.noreply.github.com>
2025-10-07 12:08:32 -0300

    07/10

--- main.js
+++ main.js
@@ -138,3 +138,13 @@ const perguntas = [
     },
 ];
 
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas{atual};
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+mostraPergunta();
