787d80a842250314ef1e9f011f6fae3c45830536
mariaeduardaaaaaaaa <146455966+mariaeduardaaaaaaaa@users.noreply.github.com>
2025-10-21 12:11:19 -0300

    21/10

--- main.js
+++ main.js
@@ -141,27 +141,41 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = " ";
 
-function mostraPergunta() {
+function mostraPergunta(){
+
+    if (atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = " ";
     mostraAlternativas();
 }
 
 function mostraAlternativas(){
-for(const alternativa of perguntaAtual.alternativas){
-    const botaoAlternativas = document.createElement("button");
-    botaoAlternativas.textContent = alternativa.texto;
-    botaoAlternativas.addEventListener("click",()=> respostaSelecionada(alternativa)){
-    caixaAlternativas.appendChild(botaoAlternativas);
-}
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    } 
 }
 
 function respostaSelecionada(opcaoSelecionada){
     const afirmacoes = opcaoSelecionada.afirmacao;
-    historiaFinal+= afirmacoes + " ";
-    atual++
+    historiaFinal += afirmacoes + " ";
+    atual++;
     mostraPergunta();
+}
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "Se fosse possível...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = " ";
 }
 
 mostraPergunta();
